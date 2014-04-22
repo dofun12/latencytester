835b07a744e872e7ea9ed319b302d1853d195360
kevim <k.v.n.such@gmail.com>
2014-04-22 13:40:35 -0300

    runs on linux

--- src/main/java/org/lemano/pingchartgenerator/Tasker.java
+++ src/main/java/org/lemano/pingchartgenerator/Tasker.java
@@ -28,10 +28,10 @@ import org.lemano.pingchartgenerator.model.Dominio;
  */
 public class Tasker extends TimerTask {
 
-    private Map<String, Integer> pico = new HashMap<String, Integer>();
-    private Map<String, Integer> status = new HashMap<String, Integer>();
+    private Map<String, Float> pico = new HashMap<String, Float>();
+    private Map<String, Float> status = new HashMap<String, Float>();
     private Map<String, Integer> perdas = new HashMap<String, Integer>();
-    private Map<String, List<Integer>> dados = new HashMap<String,List<Integer>>();
+    private Map<String, List<Float>> dados = new HashMap<String, List<Float>>();
     List<Dominio> dominios = new ArrayList<Dominio>();
     JPanel panelStatus;
 
@@ -46,7 +46,7 @@ public class Tasker extends TimerTask {
 
     @Override
     public void run() {
-        Integer rand = 0;
+        float rand = 0f;
         try {
             long millis = new Date().getTime();
             for (Dominio d : dominios) {
@@ -55,25 +55,25 @@ public class Tasker extends TimerTask {
                 if (pico.get(d.getNome()) == null) {
                     pico.put(d.getNome(), rand);
                 } else {
-                    Integer ultimo = pico.get(d.getNome());
-                    if (rand>ultimo) {
+                    Float ultimo = pico.get(d.getNome());
+                    if (rand > ultimo) {
                         pico.put(d.getNome(), rand);
                     }
                 }
-                if(rand==0){
+                if (rand == 0) {
                     Integer perda = perdas.get(d.getNome());
-                    if(perda==null){
-                        perdas.put(d.getNome(),1);
-                    }else{
-                        perdas.put(d.getNome(),perda+1);
+                    if (perda == null) {
+                        perdas.put(d.getNome(), 1);
+                    } else {
+                        perdas.put(d.getNome(), perda + 1);
                     }
                 }
-                if(dados.get(d.getNome())==null){
-                    dados.put(d.getNome(),new ArrayList<Integer>());
-                }else{
-                    if(dados.get(d.getNome()).size()>5){
-                        Integer media = getMedia(dados.get(d.getNome())).intValue();
-                        dados.put(d.getNome(),new ArrayList<Integer>());
+                if (dados.get(d.getNome()) == null) {
+                    dados.put(d.getNome(), new ArrayList<Float>());
+                } else {
+                    if (dados.get(d.getNome()).size() > 5) {
+                        Float media = getMedia(dados.get(d.getNome()));
+                        dados.put(d.getNome(), new ArrayList<Float>());
                         dados.get(d.getNome()).add(media);
                     }
                     dados.get(d.getNome()).add(rand);
@@ -87,15 +87,14 @@ public class Tasker extends TimerTask {
         }
     }
 
-    
-    public Float getMedia(List<Integer> dados){
-        float soma=0;
-        for(Integer i:dados){
-            soma+=i;
+    public Float getMedia(List<Float> dados) {
+        float soma = 0;
+        for (Float i : dados) {
+            soma += i;
         }
-        return (soma/dados.size());
+        return (soma / dados.size());
     }
-    
+
     public void atualizaStatus(Dominio d) {
         String picoComp = "pico_" + d.getNome();
         String msComp = "txt_" + d.getNome();
@@ -105,85 +104,89 @@ public class Tasker extends TimerTask {
         for (Component c : panelStatus.getComponents()) {
             if (msComp.equals(c.getName())) {
                 JLabel l = (JLabel) c;
-                Integer latency = status.get(nomeDominio);
-                String ms = latency+"ms";
+                Float latency = status.get(nomeDominio);
+                String ms = latency + "ms";
                 l.setForeground(getDinamicColor(latency));
                 l.setText(ms);
-            }else if (picoComp.equals(c.getName())) {
+            } else if (picoComp.equals(c.getName())) {
                 JLabel l = (JLabel) c;
-                Integer latency = pico.get(nomeDominio);
-                String ms = latency+"ms";
+                Float latency = pico.get(nomeDominio);
+                String ms = latency + "ms";
                 l.setForeground(getDinamicColor(latency));
                 l.setText(ms);
-            }else if (perdaComp.equals(c.getName())) {
+            } else if (perdaComp.equals(c.getName())) {
                 JLabel l = (JLabel) c;
                 Integer perda = perdas.get(nomeDominio);
                 String ms = "";
-                if (perda==null) {
+                if (perda == null) {
                     ms = "0";
-                }else{
-                    ms=""+perda;
+                } else {
+                    ms = "" + perda;
                 }
                 l.setText(ms);
-            }else if (mediaComp.equals(c.getName())) {
+            } else if (mediaComp.equals(c.getName())) {
                 JLabel l = (JLabel) c;
                 Float latency = getMedia(dados.get(nomeDominio));
                 DecimalFormat formattter = new DecimalFormat("0.00");
-                
-                String ms = formattter.format(latency)+"ms";
+
+                String ms = formattter.format(latency) + "ms";
 //                l.setForeground(getDinamicColor(latency));
                 l.setText(ms);
             }
         }
     }
 
-    public Color getDinamicColor(int value){
-        if(value<=40){
-            return new Color(32, 74,25);
-        }else{
+    public Color getDinamicColor(Float value) {
+        if (value <= 40) {
+            return new Color(32, 74, 25);
+        } else {
             return Color.RED;
         }
     }
-    
-    public Integer getMS(String pingResponse) {
+
+    public Float getMS(String pingResponse) {
         try {
             if (pingResponse != null && !pingResponse.isEmpty()) {
                 for (String parte : pingResponse.split(" ")) {
                     if (parte.contains("tempo")) {
                         String ms = parte.split("=")[1];
                         ms = ms.substring(0, ms.length() - 2);
-                        return Integer.parseInt(ms);
+                        return Float.parseFloat(ms);
+                    }else if (parte.contains("time")) {
+                        String ms = parte.split("=")[1];
+                        return Float.parseFloat(ms);
                     }
                 }
             } else {
-                return 0;
+                return 0f;
             }
-            return 0;
+            return 0f;
         } catch (Exception e) {
-            return 0;
+            return 0f;
         }
     }
 
     private String pingGetResponse(String host) throws IOException,
             InterruptedException {
-        // boolean isWindows =
-        // System.getProperty("os.name").toLowerCase().contains("win");
-
-        ProcessBuilder processBuilder = new ProcessBuilder("ping", "-n", "1",
-                "-l", "10", host);
+        boolean isWindows = System.getProperty("os.name").toLowerCase().contains("win");
+        ProcessBuilder processBuilder;
+        if (isWindows) {
+            processBuilder = new ProcessBuilder("ping", "-n", "1",
+                    "-l", "10", host);
+        }else{
+            processBuilder = new ProcessBuilder("ping", "-c", "1", host);
+        }
         Process proc = processBuilder.start();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(
                 proc.getInputStream()));
         StringBuilder builder = new StringBuilder();
         String line = null;
         while ((line = br.readLine()) != null) {
             if (line.contains("Resposta")) {
-                builder.append("(" + new Date() + ") " + line);
+                builder.append(line);
                 break;
-            } else if (line.contains("Esgotado")) {
-                builder.append("(" + new Date() + ") "
-                        + "*******Sem sinal********");
+            }else if (line.contains("time=")) {
+                builder.append("(" + new Date() + ") "+line);
                 break;
             }
         }
@@ -191,20 +194,45 @@ public class Tasker extends TimerTask {
         return result;
     }
 
-    public Map<String, Integer> getPico() {
+    public Map<String, Float> getPico() {
         return pico;
     }
 
-    public void setPico(Map<String, Integer> pico) {
+    public void setPico(Map<String, Float> pico) {
         this.pico = pico;
     }
 
-    public Map<String, Integer> getStatus() {
+    public Map<String, Float> getStatus() {
         return status;
     }
 
-    public void setStatus(Map<String, Integer> status) {
+    public void setStatus(Map<String, Float> status) {
         this.status = status;
+    }
+
+  
+    public Map<String, List<Float>> getDados() {
+        return dados;
+    }
+
+    public void setDados(Map<String, List<Float>> dados) {
+        this.dados = dados;
+    }
+
+    public List<Dominio> getDominios() {
+        return dominios;
+    }
+
+    public void setDominios(List<Dominio> dominios) {
+        this.dominios = dominios;
+    }
+
+    public Map<String, Integer> getPerdas() {
+        return perdas;
+    }
+
+    public void setPerdas(Map<String, Integer> perdas) {
+        this.perdas = perdas;
     }
 
 }
